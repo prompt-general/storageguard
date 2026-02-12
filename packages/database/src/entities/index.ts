2518078d4b29a1d3e83dafda497d46fefd80b297
caleb rutto <calebrutto91@gmail.com>
2026-02-12 17:01:07 +0300

    feat: Create an entity index file to export all database entities.

---
+++ packages/database/src/entities/index.ts
@@ -0,0 +1,7 @@
+// packages/database/src/entities/index.ts
+export * from './tenant.entity';
+export * from './cloud-account.entity';
+export * from './storage-resource.entity';
+export * from './finding.entity';
+export * from './control.entity';
+export * from './remediation-action.entity';
