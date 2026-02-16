3f3ad058a05d758ca0e83a642a6ed87d7e87ddfa
caleb rutto <calebrutto91@gmail.com>
2026-02-16 13:45:06 +0300

    feat: introduce CiModule to encapsulate CI controller, services, and analyzer.

--- apps/api/src/ci/ci.module.ts
+++ apps/api/src/ci/ci.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { CiController } from './ci.controller';
 import { CiService } from './ci.service';
 import { ParserService } from './parser.service';
+import { TemplateAnalyzerService } from './analyzer.service';
+import { ControlModule } from '../control/control.module';
 
 @Module({
+    imports: [ControlModule],
     controllers: [CiController],
-    providers: [CiService, ParserService],
-    exports: [CiService],
+    providers: [CiService, ParserService, TemplateAnalyzerService],
+    exports: [CiService, TemplateAnalyzerService],
 })
+
 export class CiModule { }
